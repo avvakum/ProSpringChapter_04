50df749e2b49e5146fa87293ee5d9209884f0b2f
AVVA <abba501@mail.ru>
2025-08-25 12:31:31 +0300

    #PeaceDeathEverywhere 25.08.2025

---
+++ src/main/java/com/avva/four/impl/provider/ConfigurableMessageProvider.java
@@ -0,0 +1,20 @@
+package com.avva.four.impl.provider;
+
+import com.zpack.decoupled.MessageProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigurableMessageProvider implements MessageProvider {
+
+    private final String message;
+
+    public ConfigurableMessageProvider(@Value("Text Sample") String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
